4bdd9e523b598894ee1a05a5738adb737960a225
Ravi Teja TV <ravitejatv@Ravis-MacBook-Pro.local>
2021-05-09 23:04:56 -0400

    Added Z and T test for 2b

--- code/task2b.py
+++ code/task2b.py
@@ -5,8 +5,11 @@ class MeanTests:
 
     def __init__(self):
         self.zalpha = 1.95996
+        self.tone = 1.69726088
+        self.ttwo = 1.67155277
 
     def load_csv(self):
+        #Applying on all data since March data has been removed as part of Tukey's rule
         data_AK=pd.read_csv('../data/State_data/AK_original.csv').to_numpy()
         data_AL=pd.read_csv('../data/State_data/AL_original.csv').to_numpy()
         return data_AK, data_AL
@@ -39,6 +42,7 @@ class MeanTests:
         cnt_num = cnt_pr - cnt_gt
         cnt_den = np.sqrt(np.sum(mar_data_count))/mar_data_count.size
         cnt_w = abs(cnt_num/cnt_den)
+        print(cnt_w)
         if cnt_w<=self.zalpha:
             print("Accept that means of the counts are same between Feb'21 and Mar'21")
         else:
@@ -50,6 +54,7 @@ class MeanTests:
         deaths_num = deaths_pr - deaths_gt
         deaths_den = np.sqrt(np.sum(mar_data_deaths))/mar_data_deaths.size
         deaths_w = abs(deaths_num/deaths_den)
+        print(deaths_w)
         if deaths_w<=self.zalpha:
             print("Accept that means of the deaths are same between Feb'21 and Mar'21")
         else:
@@ -71,6 +76,7 @@ class MeanTests:
         cnt_den = np.sqrt(np.sum(feb_data_count)/np.square(feb_data_count.size) 
         + np.sum(mar_data_count)/np.square(mar_data_count.size))
         cnt_w = abs(cnt_num/cnt_den)
+        print(cnt_w)
         if cnt_w<=self.zalpha:
             print("Accept that means of the counts are same between Feb'21 and Mar'21")
         else:
@@ -84,11 +90,123 @@ class MeanTests:
         deaths_den = np.sqrt(np.sum(feb_data_deaths)/np.square(feb_data_deaths.size) 
         + np.sum(mar_data_deaths)/np.square(mar_data_deaths.size))
         deaths_w = abs(deaths_num/deaths_den)
+        print(deaths_w)
         if deaths_w<=self.zalpha:
             print("Accept that means of the deaths are same between Feb'21 and Mar'21")
         else:
             print("Reject that means of the deaths are same between Feb'21 and Mar'21")
 
+    def true_var(self, data):
+        data_count = data[:,1:2].flatten()
+        data_deaths = data[:,2:3].flatten()
+
+        var_count = np.var(data_count, ddof=1)
+        var_deaths = np.var(data_deaths, ddof=1)
+        return var_count, var_deaths
+
+    def apply_one_z(self, data, var_count, var_deaths):
+        feb_data, mar_data = self.get_months_data(data)
+        feb_data_count = feb_data[:,1:2].flatten()
+        feb_data_deaths = feb_data[:,2:3].flatten()
+
+        mar_data_count = mar_data[:,1:2].flatten()
+        mar_data_deaths = mar_data[:,2:3].flatten()
+
+        #count
+        cnt_gt = self.get_mle(feb_data_count)
+        cnt_pr = self.get_mle(mar_data_count)
+        cnt_num = cnt_pr - cnt_gt
+        cnt_den = np.sqrt(var_count/mar_data_count.size)
+        cnt_z = abs(cnt_num/cnt_den)
+        print(cnt_z)
+        if cnt_z<=self.zalpha:
+            print("Accept that means of the counts are same between Feb'21 and Mar'21")
+        else:
+            print("Reject that means of the counts are same between Feb'21 and Mar'21")
+
+        #deaths
+        deaths_gt = self.get_mle(feb_data_deaths)
+        deaths_pr = self.get_mle(mar_data_deaths)
+        deaths_num = deaths_pr - deaths_gt
+        deaths_den = np.sqrt(var_deaths/mar_data_deaths.size)
+        deaths_z = abs(deaths_num/deaths_den)
+        print(deaths_z)
+        if deaths_z<=self.zalpha:
+            print("Accept that means of the deaths are same between Feb'21 and Mar'21")
+        else:
+            print("Reject that means of the deaths are same between Feb'21 and Mar'21")
+    
+
+    def apply_one_t(self, data):
+        feb_data, mar_data = self.get_months_data(data)
+        feb_data_count = feb_data[:,1:2].flatten()
+        feb_data_deaths = feb_data[:,2:3].flatten()
+
+        mar_data_count = mar_data[:,1:2].flatten()
+        mar_data_deaths = mar_data[:,2:3].flatten()
+        t_var_count, t_var_deaths = self.true_var(mar_data)
+
+        #count
+        cnt_gt = self.get_mle(feb_data_count)
+        cnt_pr = self.get_mle(mar_data_count)
+        cnt_num = cnt_pr - cnt_gt
+        cnt_den = np.sqrt(t_var_count/mar_data_count.size)
+        cnt_t = abs(cnt_num/cnt_den)
+        print(cnt_t)
+        if cnt_t<=self.tone:
+            print("Accept that means of the counts are same between Feb'21 and Mar'21")
+        else:
+            print("Reject that means of the counts are same between Feb'21 and Mar'21")
+
+        #deaths
+        deaths_gt = self.get_mle(feb_data_deaths)
+        deaths_pr = self.get_mle(mar_data_deaths)
+        deaths_num = deaths_pr - deaths_gt
+        deaths_den = np.sqrt(t_var_deaths/mar_data_deaths.size)
+        deaths_t = abs(deaths_num/deaths_den)
+        print(deaths_t)
+        if deaths_t<=self.tone:
+            print("Accept that means of the deaths are same between Feb'21 and Mar'21")
+        else:
+            print("Reject that means of the deaths are same between Feb'21 and Mar'21")
+
+    
+    def apply_two_t(self, data):
+        feb_data, mar_data = self.get_months_data(data)
+        feb_data_count = feb_data[:,1:2].flatten()
+        feb_data_deaths = feb_data[:,2:3].flatten()
+
+        mar_data_count = mar_data[:,1:2].flatten()
+        mar_data_deaths = mar_data[:,2:3].flatten()
+
+        t_feb_var_count, t_feb_var_deaths = self.true_var(feb_data)
+        t_mar_var_count, t_mar_var_deaths = self.true_var(mar_data)
+        #counts 
+        cnt_gt = 0
+        cnt_pr = self.get_mle(feb_data_count) - self.get_mle(mar_data_count)
+        cnt_num = cnt_pr - cnt_gt
+        cnt_den = np.sqrt((t_feb_var_count/feb_data_count.size)
+        + (t_mar_var_count/mar_data_deaths.size))
+        cnt_t = abs(cnt_num/cnt_den)
+        print(cnt_t)
+        if cnt_t<=self.ttwo:
+            print("Accept that means of the counts are same between Feb'21 and Mar'21")
+        else:
+            print("Reject that means of the counts are same between Feb'21 and Mar'21")
+
+
+        #deaths
+        deaths_gt = 0
+        deaths_pr = self.get_mle(feb_data_deaths) - self.get_mle(mar_data_deaths)
+        deaths_num = deaths_pr - deaths_gt
+        deaths_den = np.sqrt((t_feb_var_deaths/feb_data_deaths.size)
+        + (t_mar_var_deaths/mar_data_deaths.size))
+        deaths_t = abs(deaths_num/deaths_den)
+        print(deaths_t)
+        if deaths_t<=self.ttwo:
+            print("Accept that means of the deaths are same between Feb'21 and Mar'21")
+        else:
+            print("Reject that means of the deaths are same between Feb'21 and Mar'21")
 
         
 
@@ -106,12 +224,41 @@ print("Two sample Wald's")
 print("")
 meanTest.apply_two_walds(data_AK)
 print("")
-# print("######################################################")
-# print("For State AL")
-# print("######################################################")
-# print("")
-# meanTest.apply_one_walds(data_AL)
-# print("")
-# print("Two sample Wald's")
-# print("")
-# meanTest.apply_two_walds(data_AL)
+print("One sample Z")
+print("")
+var_count, var_deaths = meanTest.true_var(data_AK)
+meanTest.apply_one_z(data_AK, var_count, var_deaths)
+print("")
+print("One sample T")
+print("")
+meanTest.apply_one_t(data_AK)
+print("")
+print("Two sample T")
+print("")
+meanTest.apply_two_t(data_AK)
+print("")
+
+
+print("######################################################")
+print("For State AL")
+print("######################################################")
+print("")
+meanTest.apply_one_walds(data_AL)
+print("")
+print("Two sample Wald's")
+print("")
+meanTest.apply_two_walds(data_AL)
+print("")
+print("One sample Z")
+print("")
+var_count, var_deaths = meanTest.true_var(data_AL)
+meanTest.apply_one_z(data_AL, var_count, var_deaths)
+print("")
+print("One sample T")
+print("")
+meanTest.apply_one_t(data_AL)
+print("")
+print("Two sample T")
+print("")
+meanTest.apply_two_t(data_AL)
+print("")
