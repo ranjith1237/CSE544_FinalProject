df1686d0d216ec647d0f27c61efa77aeb618c70b
Manish Reddy <max@Maxs-MacBook-Pro.local>
2021-05-14 15:32:31 -0400

    changes to x codes

--- code/taskX1.py
+++ code/taskX1.py
@@ -1,41 +1,81 @@
 import numpy as np
+import pandas as pd
 import scipy.stats as stats
+import os
 
-def get_QObs(o1, o2, e1, e2):
-	a = ((e1 - o1)**2)/e1
-	print("a value", a)
-	b = ((e2 - o2)**2)/e2
-	print("b value", b)
-	return np.sum((a,b))
+class chi_square_test():
+	def get_data(self, path, col, start_date, end_date):
+		path=os.path.join(path,"X_Processed_Final.csv")
+		df = pd.read_csv(path)
+		after_start_date = df["Date"] >= start_date
+		before_end_date = df["Date"] <= end_date
+		between_two_dates = after_start_date & before_end_date
+		filtered_data = df.loc[between_two_dates]
+		#print("filtered_data",filtered_data)
+		test_data=filtered_data[col].values
+		return test_data
 
-if __name__ == '__main__':
-	# data_2019 = np.array([1,2,3,4,5])
-	# data_2020 = np.array([2,3,4,5,6])
-	data_2019 = np.array([40,30])
-	data_2020 = np.array([100,50])
+def get_QObs(o,e):
+	a = ((e - o)**2)/e
+	#print("a value", a)
+	return np.sum(a)
+
+def check_chi_square_test(path, cols):
+	chi1 = chi_square_test()
+	start_date = "2020-03-14"
+	end_date = "2020-04-13"
+	a1 = chi1.get_data(path, cols[0], start_date, end_date)
+	a2 = chi1.get_data(path, cols[1], start_date, end_date)
+	# a3 = chi1.get_data(path, cols[0], "01-02-2021", "28-02-2021")
+	# a4 = chi1.get_data(path, cols[1], "01-02-2021", "28-02-2021")
+
+	# print(a1.shape)
+	# print(a2.shape)
+	# data1 = np.array([np.mean(a1), np.mean(a2)])
+	# data2 = np.array([np.mean(a3), np.mean(a4)])
+
+	#data = np.vstack([np.hstack([a1.reshape(-1,1),a2.reshape(-1,1)]), np.hstack([a3.reshape(-1,1),a4.reshape(-1,1)])])
+	data = np.hstack(([a1.reshape(-1,1),a2.reshape(-1,1)]))
+	print(data)
 	#compute Q_obs
 	##find the expectations
-	total_years = np.sum((data_2019, data_2020), axis = 0)
-	total_atts =  np.sum((data_2019, data_2020), axis = 1)
+	total_months = np.sum(data, axis = 0)
+	total_atts =  np.sum(data, axis = 1)
 
-	data_2019E = total_years * (total_atts[0]/np.sum(total_atts))
-	data_2020E = total_years * (total_atts[1]/np.sum(total_atts))
+	dataE = []
+	for i in range(len(total_atts)):
+		dataE.append(total_months * (total_atts[i]/np.sum(total_atts)))
+	# data1E = total_months * (total_atts[0]/np.sum(total_atts))
+	# data2E = total_months * (total_atts[1]/np.sum(total_atts))
 
-	print(data_2019E)
-	print(data_2020E)
+	#print(dataE)
+	# print(data2E)
 
-	Q_obs = get_QObs(data_2019, data_2020, data_2019E, data_2020E)
+	Q_obs = get_QObs(data, dataE)
 	print("Chi-Square stat", Q_obs)
-	df = data_2020.shape[0] - 1
+	df = data.shape[0] - 1
 
 	#get the p-value from t-table
-	crit = stats.chi2.ppf(q=0.95, df=1)
+	print("Degrees of freedom =>", df)
+	crit = stats.chi2.ppf(q=0.95, df=df)
 	print("critical value", crit)
 
 	if Q_obs < crit:
 		print('Independent')
 	else:
 		print('Not Independent')
+
+
+if __name__ == '__main__':
+	# data_2019 = np.array([1,2,3,4,5])
+	# data_2020 = np.array([2,3,4,5,6])
+	# data_2019 = np.array([40,30])
+	# data_2020 = np.array([100,50])
+	
+	path="../data/X_data"
+	cols=["Crashes","Cases"]
+	check_chi_square_test(path, cols)
+	
 
 
 
--- code/taskX2.py
+++ code/taskX2.py
@@ -1,4 +1,17 @@
 import numpy as np
+import pandas as pd
+import os
+
+class pearson_correlaton:
+	def get_data(self, path, col, start_date, end_date):
+		path=os.path.join(path,"X_Processed_Final.csv")
+		df = pd.read_csv(path)
+		after_start_date = df["Date"] >= start_date
+		before_end_date = df["Date"] <= end_date
+		between_two_dates = after_start_date & before_end_date
+		filtered_data = df.loc[between_two_dates]
+		test_data=filtered_data[col].values
+		return test_data
 
 def check_correlation(val, crit):
 	if np.abs(val) < crit:
@@ -6,24 +19,35 @@ def check_correlation(val, crit):
 	elif val > (1-crit):
 		print("Correlated")
 	else:
-		print("Not Correlated") 
+		print("Not Correlated")
 
 def cal_pearson(A,B):
 	return (np.mean(A*B) - np.mean(A)*np.mean(B))/(np.std(A)*np.std(B))
 
 if __name__ == '__main__':
-	A = np.array([48,40,58,53,65,25,52,34,30,45])
-	B = np.array([54,48,51,47,62,35,70,20,25,40])
-	C = np.array([19,40,35,41,38,32,32,37,37,15])
-	crit = 0.05
+	# A = np.array([48,40,58,53,65,25,52,34,30,45])
+	# B = np.array([54,48,51,47,62,35,70,20,25,40])
+	# C = np.array([19,40,35,41,38,32,32,37,37,15])
+	crit = 0.1
 	
-	l = cal_pearson(A,B)
-	m = cal_pearson(A,C)
-	n = cal_pearson(B,C)
+	path="../data/X_data"
+	cols=["Crashes","Cases"]
+	peace = pearson_correlaton()
+	start_date = "2020-01-1"
+	end_date = "2021-01-31"
+	collisions = peace.get_data(path, cols[0], start_date, end_date)
+	cases = peace.get_data(path, cols[1], start_date, end_date)
 
-	check_correlation(l, crit)
-	check_correlation(m, crit)
-	check_correlation(n, crit)
+	gg = cal_pearson(collisions, cases)
+	print("Sxy-value => ", gg)
+	# l = cal_pearson(A,B)
+	# m = cal_pearson(A,C)
+	# n = cal_pearson(B,C)
+
+	check_correlation(gg, crit)
+	# check_correlation(l, crit)
+	# check_correlation(m, crit)
+	# check_correlation(n, crit)
 
 	
 
